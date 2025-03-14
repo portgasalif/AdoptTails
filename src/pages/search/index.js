226312d33a764306cb4424ca96ab8173d6393cf5
Alif Fachrel Anargya <aliffachrel@gmail.com>
2025-03-14 15:03:17 +0700

    Remove unused navigate import from SearchPage component

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -2,10 +2,9 @@ import React, { useState, useEffect } from "react";
 import Hero from "../../components/hero";
 import { getPets } from "../../api/petfinder";
 import Pet from "../../components/pet";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 const SearchPage = () => {
-  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const petNameToFind = searchParams.get("name");
 
